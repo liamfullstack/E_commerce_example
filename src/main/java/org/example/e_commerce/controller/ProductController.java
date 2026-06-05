c376ec509cb9b2bf32f357ec8004ae6ecdd726e0
liamfullstack <liamlu1348@icloud.com>
2026-06-05 12:51:47 +0800

    商品功能 -實作修改商品

--- src/main/java/org/example/e_commerce/controller/ProductController.java
+++ src/main/java/org/example/e_commerce/controller/ProductController.java
@@ -1,6 +1,7 @@
 package org.example.e_commerce.controller;
 
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.example.e_commerce.dto.ProductRequest;
 import org.example.e_commerce.model.Product;
 import org.example.e_commerce.service.ProductService;
@@ -32,5 +33,23 @@ public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
         Product product = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        //檢查Product是否存在
+        Product product = productService.getProductById(productId);
+
+        if(product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        //修改商品的數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updatedProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
 }
